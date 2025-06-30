b666a00ccaf44c6a821c5f0608e2cbb506876ef1
Didier Nsengiyumva <nsedidier@gmail.com>
2025-06-30 21:51:02 +0200

    Latest updates 950

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,40 +1,34 @@
 // src/game/game.module.ts
 
 
+// src/game/game.module.ts
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { RedisModule } from '../redis/redis.module';
+import { GameController } from './game.controller';
+import { GameService } from './game.service';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/ludo'),
-    RedisModule,
-  ],
+  controllers: [GameController],
+  providers: [GameService],
+  exports: [GameService], 
 })
-export class AppModule {}
-
-
-
+export class GameModule {}
 
 
 
 
 // import { Module } from '@nestjs/common';
-// import { GameGateway } from './game.gateway';
-// import { GameService } from './game.service';
 // import { MongooseModule } from '@nestjs/mongoose';
-// import { GameRoom, GameRoomSchema } from './schemas/game-room.schema';
-// import { GameSessionEntity, GameSessionSchema } from './schemas/game-session.schema';
 // import { RedisModule } from '../redis/redis.module';
 
 // @Module({
 //   imports: [
-//     MongooseModule.forFeature([
-//       { name: GameRoom.name, schema: GameRoomSchema },
-//       { name: GameSessionEntity.name, schema: GameSessionSchema },
-//     ]),
+//     MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/ludo'),
 //     RedisModule,
 //   ],
-//   providers: [GameGateway, GameService],
 // })
-// export class GameModule {}
+// export class AppModule {}
+
+
+
+
+
