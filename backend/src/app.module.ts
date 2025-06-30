3079bb4902703476417e0fdfb65b2cff3c6f0890
Didier Nsengiyumva <nsedidier@gmail.com>
2025-06-30 21:54:41 +0200

    latest updates 954

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,12 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RedisModule } from './redis/redis.module';
+import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb+srv://nsedidier:zxqWjmMu7RYg7u0B@cluster0.eopcqfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"), // e.g. mongodb://localhost:27017/ludo
-    // RedisModule.forRoot({
-    //   config: {
-    //     url: process.env.REDIS_URL ?? 'redis://localhost:6379',
-    //   },
-    // }),
-    RedisModule,
+    GameModule,
   ],
 })
 export class AppModule {}
