325f3df49446a7612879c05620a67bc089ed49a4
Didier Nsengiyumva <nsedidier@gmail.com>
2025-07-21 08:05:00 +0200

    latest updates 804

--- turn-server/server.js
+++ turn-server/server.js
@@ -46,17 +46,3 @@ server.on('error', function(err) {
 
 
 
-
-// const { NodeTURN } = require('node-turn');
-
-// const server = new NodeTURN({
-//   listeningPort: 3478,
-//   authMech: 'long-term',
-//   credentials: {
-//     username: "aluglobe2025",
-//     password: "aluglobe2025development"
-//   },
-//   debugLevel: 'ALL'
-// });
-
-// server.start();
