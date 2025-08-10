e2c7ec6a39f28b5a527f9798aa94d67a889fadc6
Didier Nsengiyumva <nsedidier@gmail.com>
2025-08-10 13:43:06 +0200

    latest updates 143

--- frontend/src/components/Ludo/src-ludo/containers/Ludo/Container.tsx
+++ frontend/src/components/Ludo/src-ludo/containers/Ludo/Container.tsx
@@ -1,174 +1,3 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import { Base } from '../../containers/Base/Container';
-import { Home } from '../../containers/Home/Container';
-import { Player } from '../../containers/Player/Container';
-import { Walkway } from '../../containers/Walkway/Container';
-import { getStyleObject } from '../../containers/utils';
-import { BOARD_SIZE } from '../../globalConstants';
-import { ContextMenu } from '../../services/contextMenu/Container';
-
-import { getInitialGameData, setPlayers } from './state/actions';
-import { BaseID, BoardEntities } from './state/interfaces';
-import {
-  basesSelector,
-  currentTurnSelector,
-  relationshipsSelector,
-  walkwaysSelector,
-} from './state/selectors';
-
-import styles from './Container.module.css';
-
-interface IDispatchProps {
-  getInitialGameData: typeof getInitialGameData;
-  setPlayers: typeof setPlayers;
-}
-
-interface IStateProps {
-  bases: ReturnType<typeof basesSelector>;
-  relationships: ReturnType<typeof relationshipsSelector>;
-  walkways: ReturnType<typeof walkwaysSelector>;
-  currentTurn: ReturnType<typeof currentTurnSelector>;
-}
-
-interface IPublicProps {
-
-}
-
-interface IProps extends IPublicProps, IStateProps, IDispatchProps {}
-
-interface IState {
-  showPlayerConfiguration: boolean;
-}
-
-const mapStateToProps = createStructuredSelector({
-  bases: basesSelector,
-  currentTurn: currentTurnSelector,
-  relationships: relationshipsSelector,
-  walkways: walkwaysSelector,
-});
-
-const mapDispatchToProps = {
-  setPlayers,
-  getInitialGameData,
-};
-
-class LudoBare extends React.PureComponent<IProps, IState> {
-  state: IState = { showPlayerConfiguration: true }
-
-  componentDidMount() {
-    this.props.getInitialGameData();
-  }
-
-  render() {
-    const { currentTurn } = this.props;
-    return (
-      <div className={styles.Container}>
-        <div className={styles.GameContainer}>
-          {/* Classic Ludo 3x3 Grid Layout */}
-          <div className={styles.LudoGrid}>
-            {/* Top Row */}
-            <div className={styles.TopLeft}>
-              <Base baseID={BaseID.BASE_1} enabled={currentTurn === BaseID.BASE_1} hasWon={false}/>
-            </div>
-            <div className={styles.TopCenter}>
-              <Player baseID={BaseID.BASE_1} placement='top' disabled={currentTurn !== BaseID.BASE_1}/>
-            </div>
-            <div className={styles.TopRight}>
-              <Base baseID={BaseID.BASE_2} enabled={currentTurn === BaseID.BASE_2} hasWon={false}/>
-            </div>
-
-            {/* Middle Row */}
-            <div className={styles.MiddleLeft}>
-              <Player baseID={BaseID.BASE_4} placement='top' disabled={currentTurn !== BaseID.BASE_4}/>
-            </div>
-            <div className={styles.Board} style={getStyleObject(BOARD_SIZE, BOARD_SIZE)}>
-              {this.renderBoardEntities()}
-            </div>
-            <div className={styles.MiddleRight}>
-              <Player baseID={BaseID.BASE_2} placement='top' disabled={currentTurn !== BaseID.BASE_2}/>
-            </div>
-
-            {/* Bottom Row */}
-            <div className={styles.BottomLeft}>
-              <Base baseID={BaseID.BASE_4} enabled={currentTurn === BaseID.BASE_4} hasWon={false}/>
-            </div>
-            <div className={styles.BottomCenter}>
-              <Player baseID={BaseID.BASE_3} placement='bottom' disabled={currentTurn !== BaseID.BASE_3}/>
-            </div>
-            <div className={styles.BottomRight}>
-              <Base baseID={BaseID.BASE_3} enabled={currentTurn === BaseID.BASE_3} hasWon={false}/>
-            </div>
-          </div>
-        </div>
-        {
-          this.state.showPlayerConfiguration
-          ? (
-            <div className={styles.GameConfiguration}>
-            <button className={styles.Button} onClick={() => this.startGame(2)}>2 Players</button>
-            <button className={styles.Button} onClick={() => this.startGame(3)}>3 Players</button>
-            <button className={styles.Button} onClick={() => this.startGame(4)}>4 Players</button>
-          </div>
-          )
-          : null
-        }
-        {
-          process.env.NODE_ENV === 'development'
-          ? <ContextMenu />
-          : null
-        }
-      </div>
-    );
-  }
-
-  private renderBoardEntities = () => {
-    const {
-      bases,
-      relationships,
-      walkways,
-    } = this.props;
-  
-    // Only check if relationships exist, don't be too strict
-    if (!relationships || relationships.length === 0) {
-      console.log('No relationships found, rendering empty board');
-      return null;
-    }
-  
-    return relationships.map((relationship, index) => {
-      const base = bases?.[relationship.ID];
-      const walkway = walkways?.[relationship.ID];
-      
-      switch (relationship.type) {
-        case BoardEntities.BASE:
-          // Don't render bases here - they are rendered in the corners
-          return null;
-        case BoardEntities.HOME:
-          return <Home baseIDs={relationship.baseIDs} key={index}/>;
-        case BoardEntities.WALKWAY:
-          if (!walkway) {
-            console.log(`Walkway not found for ID: ${relationship.ID}`);
-            return null;
-          }
-          return <Walkway walkway={walkway} key={index}/>;
-        default:
-          console.log(`Unknown board entity type: ${relationship.type}`);
-          return null;
-      }
-    });
-  }
-
-  private startGame = (playerCount: number) => {
-    this.props.setPlayers(playerCount);
-    this.setState({
-      showPlayerConfiguration: false,
-    });
-  }
-}
-
-export const Ludo = connect(mapStateToProps, mapDispatchToProps)(LudoBare) as unknown as React.ComponentClass<IPublicProps>;
-
 // import React from 'react';
 // import { connect } from 'react-redux';
 // import { createStructuredSelector } from 'reselect';
@@ -238,18 +67,40 @@ export const Ludo = connect(mapStateToProps, mapDispatchToProps)(LudoBare) as un
 //     return (
 //       <div className={styles.Container}>
 //         <div className={styles.GameContainer}>
-//           <div className={styles.PlayerContainer}>
-//             <Player baseID={BaseID.BASE_1} placement='top' disabled={currentTurn !== BaseID.BASE_1}/>
-//             <Player baseID={BaseID.BASE_3} placement='bottom' disabled={currentTurn !== BaseID.BASE_3}/>
-//           </div>
-//           <div className={styles.Board} style={getStyleObject(BOARD_SIZE, BOARD_SIZE)}>
-//             {
-//               this.renderBoardEntities()
-//             }
-//           </div>
-//           <div className={styles.PlayerContainer}>
-//             <Player baseID={BaseID.BASE_2} placement='top' disabled={currentTurn !== BaseID.BASE_2}/>
-//             <Player baseID={BaseID.BASE_4} placement='bottom' disabled={currentTurn !== BaseID.BASE_4}/>
+//           {/* Classic Ludo 3x3 Grid Layout */}
+//           <div className={styles.LudoGrid}>
+//             {/* Top Row */}
+//             <div className={styles.TopLeft}>
+//               <Base baseID={BaseID.BASE_1} enabled={currentTurn === BaseID.BASE_1} hasWon={false}/>
+//             </div>
+//             <div className={styles.TopCenter}>
+//               <Player baseID={BaseID.BASE_1} placement='top' disabled={currentTurn !== BaseID.BASE_1}/>
+//             </div>
+//             <div className={styles.TopRight}>
+//               <Base baseID={BaseID.BASE_2} enabled={currentTurn === BaseID.BASE_2} hasWon={false}/>
+//             </div>
+
+//             {/* Middle Row */}
+//             <div className={styles.MiddleLeft}>
+//               <Player baseID={BaseID.BASE_4} placement='top' disabled={currentTurn !== BaseID.BASE_4}/>
+//             </div>
+//             <div className={styles.Board} style={getStyleObject(BOARD_SIZE, BOARD_SIZE)}>
+//               {this.renderBoardEntities()}
+//             </div>
+//             <div className={styles.MiddleRight}>
+//               <Player baseID={BaseID.BASE_2} placement='top' disabled={currentTurn !== BaseID.BASE_2}/>
+//             </div>
+
+//             {/* Bottom Row */}
+//             <div className={styles.BottomLeft}>
+//               <Base baseID={BaseID.BASE_4} enabled={currentTurn === BaseID.BASE_4} hasWon={false}/>
+//             </div>
+//             <div className={styles.BottomCenter}>
+//               <Player baseID={BaseID.BASE_3} placement='bottom' disabled={currentTurn !== BaseID.BASE_3}/>
+//             </div>
+//             <div className={styles.BottomRight}>
+//               <Base baseID={BaseID.BASE_3} enabled={currentTurn === BaseID.BASE_3} hasWon={false}/>
+//             </div>
 //           </div>
 //         </div>
 //         {
@@ -291,11 +142,8 @@ export const Ludo = connect(mapStateToProps, mapDispatchToProps)(LudoBare) as un
       
 //       switch (relationship.type) {
 //         case BoardEntities.BASE:
-//           if (!base) {
-//             console.log(`Base not found for ID: ${relationship.ID}`);
-//             return null;
-//           }
-//           return <Base baseID={base.ID} key={index} enabled={base.enabled} hasWon={base.hasWon}/>;
+//           // Don't render bases here - they are rendered in the corners
+//           return null;
 //         case BoardEntities.HOME:
 //           return <Home baseIDs={relationship.baseIDs} key={index}/>;
 //         case BoardEntities.WALKWAY:
@@ -320,6 +168,158 @@ export const Ludo = connect(mapStateToProps, mapDispatchToProps)(LudoBare) as un
 // }
 
 // export const Ludo = connect(mapStateToProps, mapDispatchToProps)(LudoBare) as unknown as React.ComponentClass<IPublicProps>;
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { Base } from '../../containers/Base/Container';
+import { Home } from '../../containers/Home/Container';
+import { Player } from '../../containers/Player/Container';
+import { Walkway } from '../../containers/Walkway/Container';
+import { getStyleObject } from '../../containers/utils';
+import { BOARD_SIZE } from '../../globalConstants';
+import { ContextMenu } from '../../services/contextMenu/Container';
+
+import { getInitialGameData, setPlayers } from './state/actions';
+import { BaseID, BoardEntities } from './state/interfaces';
+import {
+  basesSelector,
+  currentTurnSelector,
+  relationshipsSelector,
+  walkwaysSelector,
+} from './state/selectors';
+
+import styles from './Container.module.css';
+
+interface IDispatchProps {
+  getInitialGameData: typeof getInitialGameData;
+  setPlayers: typeof setPlayers;
+}
+
+interface IStateProps {
+  bases: ReturnType<typeof basesSelector>;
+  relationships: ReturnType<typeof relationshipsSelector>;
+  walkways: ReturnType<typeof walkwaysSelector>;
+  currentTurn: ReturnType<typeof currentTurnSelector>;
+}
+
+interface IPublicProps {
+
+}
+
+interface IProps extends IPublicProps, IStateProps, IDispatchProps {}
+
+interface IState {
+  showPlayerConfiguration: boolean;
+}
+
+const mapStateToProps = createStructuredSelector({
+  bases: basesSelector,
+  currentTurn: currentTurnSelector,
+  relationships: relationshipsSelector,
+  walkways: walkwaysSelector,
+});
+
+const mapDispatchToProps = {
+  setPlayers,
+  getInitialGameData,
+};
+
+class LudoBare extends React.PureComponent<IProps, IState> {
+  state: IState = { showPlayerConfiguration: true }
+
+  componentDidMount() {
+    this.props.getInitialGameData();
+  }
+
+  render() {
+    const { currentTurn } = this.props;
+    return (
+      <div className={styles.Container}>
+        <div className={styles.GameContainer}>
+          <div className={styles.PlayerContainer}>
+            <Player baseID={BaseID.BASE_1} placement='top' disabled={currentTurn !== BaseID.BASE_1}/>
+            <Player baseID={BaseID.BASE_3} placement='bottom' disabled={currentTurn !== BaseID.BASE_3}/>
+          </div>
+          <div className={styles.Board} style={getStyleObject(BOARD_SIZE, BOARD_SIZE)}>
+            {
+              this.renderBoardEntities()
+            }
+          </div>
+          <div className={styles.PlayerContainer}>
+            <Player baseID={BaseID.BASE_2} placement='top' disabled={currentTurn !== BaseID.BASE_2}/>
+            <Player baseID={BaseID.BASE_4} placement='bottom' disabled={currentTurn !== BaseID.BASE_4}/>
+          </div>
+        </div>
+        {
+          this.state.showPlayerConfiguration
+          ? (
+            <div className={styles.GameConfiguration}>
+            <button className={styles.Button} onClick={() => this.startGame(2)}>2 Players</button>
+            <button className={styles.Button} onClick={() => this.startGame(3)}>3 Players</button>
+            <button className={styles.Button} onClick={() => this.startGame(4)}>4 Players</button>
+          </div>
+          )
+          : null
+        }
+        {
+          process.env.NODE_ENV === 'development'
+          ? <ContextMenu />
+          : null
+        }
+      </div>
+    );
+  }
+
+  private renderBoardEntities = () => {
+    const {
+      bases,
+      relationships,
+      walkways,
+    } = this.props;
+  
+    // Only check if relationships exist, don't be too strict
+    if (!relationships || relationships.length === 0) {
+      console.log('No relationships found, rendering empty board');
+      return null;
+    }
+  
+    return relationships.map((relationship, index) => {
+      const base = bases?.[relationship.ID];
+      const walkway = walkways?.[relationship.ID];
+      
+      switch (relationship.type) {
+        case BoardEntities.BASE:
+          if (!base) {
+            console.log(`Base not found for ID: ${relationship.ID}`);
+            return null;
+          }
+          return <Base baseID={base.ID} key={index} enabled={base.enabled} hasWon={base.hasWon}/>;
+        case BoardEntities.HOME:
+          return <Home baseIDs={relationship.baseIDs} key={index}/>;
+        case BoardEntities.WALKWAY:
+          if (!walkway) {
+            console.log(`Walkway not found for ID: ${relationship.ID}`);
+            return null;
+          }
+          return <Walkway walkway={walkway} key={index}/>;
+        default:
+          console.log(`Unknown board entity type: ${relationship.type}`);
+          return null;
+      }
+    });
+  }
+
+  private startGame = (playerCount: number) => {
+    this.props.setPlayers(playerCount);
+    this.setState({
+      showPlayerConfiguration: false,
+    });
+  }
+}
+
+export const Ludo = connect(mapStateToProps, mapDispatchToProps)(LudoBare) as unknown as React.ComponentClass<IPublicProps>;
 
 
 // import React from 'react';
