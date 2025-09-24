31f90fe8eb792cfa065cf08ed738115462257316
Didier Nsengiyumva <nsedidier@gmail.com>
2025-09-24 14:14:17 +0200

    latest updates 214

--- backend/src/chess/chess.module.ts
+++ backend/src/chess/chess.module.ts
@@ -16,20 +16,3 @@ import { ChessGame, ChessGameSchema } from './schemas/chess.schema';
 })
 export class ChessModule {}
 
-
-// // chess.module.ts
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { ChessService } from './chess.service';
-// import { ChessGateway } from './chess.gateway';
-// import { ChessController } from './chess.controller';
-// import { ChessGameSchema } from './schemas/chess.schema';
-
-// @Module({
-//   imports: [
-//     MongooseModule.forFeature([{ name: 'ChessGame', schema: ChessGameSchema }]),
-//   ],
-//   controllers: [ChessController],
-//   providers: [ChessService, ChessGateway],
-// })
-// export class ChessModule {}
--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -9,6 +9,8 @@ import { GameSessionEntity, GameSessionSchema } from './schemas/game-session.sch
 import { RedisModule } from '../redis/redis.module';
 import { TriviaService } from '../trivia/trivia.service';
 import { UserModule } from '../user/user.module';
+import { ChessModule } from '../chess/chess.module';
+
 @Module({
   imports: [    
     MongooseModule.forFeature([
@@ -17,6 +19,7 @@ import { UserModule } from '../user/user.module';
     ]), 
     RedisModule,
     UserModule, 
+    ChessModule,
   ],
   controllers: [GameController],
   providers: [
@@ -27,6 +30,36 @@ import { UserModule } from '../user/user.module';
   exports: [GameService],
 })
 export class GameModule {}
+
+// // src/game/game.module.ts
+// import { Module } from '@nestjs/common';
+// import { GameController } from './game.controller';
+// import { GameService } from './game.service';
+// import { GameGateway } from './game.gateway';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { GameRoom, GameRoomSchema } from './schemas/game-room.schema';
+// import { GameSessionEntity, GameSessionSchema } from './schemas/game-session.schema';
+// import { RedisModule } from '../redis/redis.module';
+// import { TriviaService } from '../trivia/trivia.service';
+// import { UserModule } from '../user/user.module';
+// @Module({
+//   imports: [    
+//     MongooseModule.forFeature([
+//       { name: GameRoom.name, schema: GameRoomSchema },
+//       { name: GameSessionEntity.name, schema: GameSessionSchema },
+//     ]), 
+//     RedisModule,
+//     UserModule, 
+//   ],
+//   controllers: [GameController],
+//   providers: [
+//     GameService, 
+//     GameGateway, 
+//     TriviaService,
+// ],
+//   exports: [GameService],
+// })
+// export class GameModule {}
 
 
 
