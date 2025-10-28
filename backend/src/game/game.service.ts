a496d4ea579890366eb0ad7a30b8403e68b0c6bf
Didier Nsengiyumva <nsedidier@gmail.com>
2025-10-28 14:07:10 +0200

    latest updates 207

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -145,7 +145,7 @@ export class GameService {
     [7, 0], [6, 0],
   ];
 
-  private startPositions: number[] = [1, 1, 1, 1];
+  private startPositions: number[] = [1, 1, 1, 14];
 
   private safePositions = [1, 9, 14, 22, 27, 35, 40, 48];
   private homeColumnStart = 52; 
